8e1ce9992ebf105c97192d49a1c682eef45412b2
xiaoyun.zhang <xiaoyun.zhang@funplus.com>
2023-02-22 17:14:53 +0800

    shoot

--- Assets/Scripts/Authoring/TurretAuthoring.cs
+++ Assets/Scripts/Authoring/TurretAuthoring.cs
@@ -4,13 +4,22 @@ using Unity.Entities;
 // They constitute the inputs for the baking systems which generates ECS data.
 class TurretAuthoring : UnityEngine.MonoBehaviour
 {
+    public UnityEngine.GameObject CannonBallPrefab;
+    public UnityEngine.Transform CannonBallSpawn;
     // Bakers convert authoring MonoBehaviours into entities and components.
     // (Nesting a baker in its associated Authoring component is not necessary but is a common convention.)
     class TurretBaker : Baker<TurretAuthoring>
     {
         public override void Bake(TurretAuthoring authoring)
         {
-            AddComponent<Turret>();
+            //AddComponent<Turret>();
+            AddComponent(new Turret
+            {
+                // By default, each authoring GameObject turns into an Entity.
+                // Given a GameObject (or authoring component), GetEntity looks up the resulting Entity.
+                CannonBallPrefab = GetEntity(authoring.CannonBallPrefab),
+                CannonBallSpawn = GetEntity(authoring.CannonBallSpawn)
+            });
         }
     }
 }
@@ -19,4 +28,8 @@ class TurretAuthoring : UnityEngine.MonoBehaviour
 // An empty component is called a "tag component".
 struct Turret : IComponentData
 {
+    // This entity will reference the nozzle of the cannon, where cannon balls should be spawned.
+    public Entity CannonBallSpawn;
+    // This entity will reference the prefab to be spawned every time the cannon shoots.
+    public Entity CannonBallPrefab;
 }
